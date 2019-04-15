a0996a9d2703d2c6a4a06625257a7fcf4a2e3a2d
sloanzhang <47101474@qq.com>
2019-04-15 13:34:49 +0800

    project prepair

---
+++ javaargs/src/com/code/args/Agrs.java
@@ -0,0 +1,8 @@
+package com.code.args;
+
+public class Agrs {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ javaargs/test/com/code/args/ArgsTest.java
@@ -0,0 +1,7 @@
+package com.code.args;
+
+/**
+ * Created by sloanzhang on 2019/4/15.
+ */
+public class ArgsTest {
+}
