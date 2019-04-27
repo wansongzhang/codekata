bb83dfc2c728f1797b787d52776249f977290423
sloanzhang <47101474@qq.com>
2019-04-27 20:48:59 +0800

    增加最外层题目的原始输入用例

--- marsrover/src/com/kata/marsrover/MarsRover.java
+++ marsrover/src/com/kata/marsrover/MarsRover.java
@@ -10,11 +10,26 @@ import com.kata.marsrover.exception.PositionException;
 public class MarsRover {
   private MarsMap marsMap;
 
-  public void readMapInfo(String mapStr) throws MapException {
+
+  public String readInput(String inputStr) throws MapException, ActionException, PositionException {
+    String []lineArray=getLineArray(inputStr);
+    readMapInfo(lineArray[0]);
+    StringBuilder sBuffer=new StringBuilder();
+    for(int i=1;i<lineArray.length;i=i+2){
+      sBuffer.append(sendCarWaitResult(lineArray[i],lineArray[i+1])).append("\r\n");
+    }
+    return sBuffer.toString();
+  }
+
+  private String[] getLineArray(String inputStr) {
+    return inputStr.trim().split("\n");
+  }
+
+  void readMapInfo(String mapStr) throws MapException {
     this.marsMap=new MarsMap(mapStr);
   }
 
-  public String sendCar(String posStr, String cmdStr) throws PositionException, ActionException {
+  String sendCarWaitResult(String posStr, String cmdStr) throws PositionException, ActionException {
     return new Car(posStr,marsMap).rover(cmdStr);
   }
 }
--- marsrover/test/com/kata/marsrover/MarsRoverTest.java
+++ marsrover/test/com/kata/marsrover/MarsRoverTest.java
@@ -18,7 +18,7 @@ public class MarsRoverTest {
     marsRover.readMapInfo("5 5");
     String posStr = "5 5 N";
     String cmdStr = "M";
-    String result = marsRover.sendCar(posStr, cmdStr);
+    String result = marsRover.sendCarWaitResult(posStr, cmdStr);
     Assert.assertEquals("5 5 N RIP", result);
   }
 
@@ -29,22 +29,30 @@ public class MarsRoverTest {
     marsRover.readMapInfo("5 5");
     String posStr = "5 5 N";
     String cmdStr = "M";
-    String result = marsRover.sendCar(posStr, cmdStr);
+    String result = marsRover.sendCarWaitResult(posStr, cmdStr);
     Assert.assertEquals("5 5 N RIP", result);
 
     cmdStr = "M";
-    result = marsRover.sendCar(posStr, cmdStr);
+    result = marsRover.sendCarWaitResult(posStr, cmdStr);
     Assert.assertEquals("5 5 N", result);
 
     cmdStr = "MLLM";
-    result = marsRover.sendCar(posStr, cmdStr);
+    result = marsRover.sendCarWaitResult(posStr, cmdStr);
     Assert.assertEquals("5 4 S", result);
 
     cmdStr = "RMM";
-    result = marsRover.sendCar(posStr, cmdStr);
+    result = marsRover.sendCarWaitResult(posStr, cmdStr);
     Assert.assertEquals("5 5 E RIP", result);
-
   }
+
+  @Test
+  public void testAll() throws ActionException, PositionException, MapException {
+    String inputString="5 5 \n 1 2 N \n LMLMLMLMM \n 3 3 E \n MMM";
+    String actualResult=new MarsRover().readInput(inputString);
+    String expectResult="1 3 N\r\n5 3 E RIP\r\n";
+    Assert.assertEquals(expectResult,actualResult);
+  }
+
 
 
 }
